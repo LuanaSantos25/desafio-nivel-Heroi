e701947b0730dfb70e4d0317e7f7fc5085ddc177
LuanaSantos25 <luhlins.santos68@gmail.com>
2024-02-17 00:07:09 -0300

    meu primeiro desafio

---
+++ NivelHeroi.js
@@ -0,0 +1,35 @@
+console.log("Bem-vindo ao jogo");
+console.log("Consulta de XP");
+
+let nomeHeroi = prompt("Digite seu nome:");
+let xpHeroi = parseInt(prompt("Digite a quantidade de XP para saber seu nível:")); 
+
+let nivel;
+
+switch (true) {
+    case xpHeroi <= 1000:
+        nivel = "Ferro";
+        break;
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        nivel = "Bronze";
+        break;
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+        nivel = "Prata";
+        break;
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
+        nivel = "Ouro";
+        break;
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+        nivel = "Platina";
+        break;
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+        nivel = "Ascendente";
+        break;
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
+}
+
+console.log("Nome do herói: " + nomeHeroi + ", você está no nível: " + nivel);
